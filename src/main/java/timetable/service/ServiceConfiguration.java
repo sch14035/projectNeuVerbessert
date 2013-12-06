7b38c25fb388a80f9594fe59cc03b61fd03c9254
sch14035 <sch14035@spengergasse.at>
2013-12-06 03:31:19 +0100

    Verbesserte Abgabe

---
+++ src/main/java/timetable/service/ServiceConfiguration.java
@@ -0,0 +1,14 @@
+/*
+ * Jumio Inc.
+ *
+ * Copyright (C) 2010 - 2011
+ * All rights reserved.
+ */
+package timetable.service;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "at.grueneis.timetable.service")
+public class ServiceConfiguration {}
